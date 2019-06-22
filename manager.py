8f7b27b643709eb84fea795dced4213e76d3d0b6
carbotaniuman <quy.d.x.nguyen@gmail.com>
2019-06-21 23:13:23 -0500

    secoind

--- manager.py
+++ manager.py
@@ -4,20 +4,16 @@ from function import Function
 
 
 class Manager:
-    idDict: Dict[str, type]
+    idDict: Dict[str, type[Function]]
 
     def __init__(self):
         self.idDict = {}
 
 # TODO: Same name collision
-    def register(self, clazz: type):
+    def register(self, clazz: type[Function]):
         self.idDict[clazz.__name__] = clazz
 
     def create(self, name: str, settings):
         return self.idDict[name](settings)
 
 
-m = Manager()
-m.register(Function)
-z = m.create("Function", 1)
-print(getattr(z, "settings"))
