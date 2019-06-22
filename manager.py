8a9c154e28618b8522d686d40bafc0b8ca0b1b59
carbotaniuman <quy.d.x.nguyen@gmail.com>
2019-06-21 23:17:43 -0500

    Fix typing

--- manager.py
+++ manager.py
@@ -1,16 +1,16 @@
-from typing import Dict
+from typing import Dict, Type
 
 from function import Function
 
 
 class Manager:
-    idDict: Dict[str, type[Function]]
+    idDict: Dict[str, Type[Function]]
 
     def __init__(self):
         self.idDict = {}
 
 # TODO: Same name collision
-    def register(self, clazz: type[Function]):
+    def register(self, clazz: Type[Function]):
         self.idDict[clazz.__name__] = clazz
 
     def create(self, name: str, settings):
