f82868b43bfac023c8e9e4ce66987bbb282a05b9
carbotaniuman <quy.d.x.nguyen@gmail.com>
2019-07-06 22:42:55 -0500

    Work on Settings, Inputs, Outputs

--- function.py
+++ function.py
@@ -1,17 +1,47 @@
-import abc
+from abc import ABC, abstractmethod
+from dataclasses import is_dataclass
+from typing import Iterable, TypeVar, Generic
+import pydantic
 
 
-class Function(abc.ABC):
-    @staticmethod
-    @abc.abstractmethod
-    def test():
-        pass
+def is_basemodel(cls):
+    return isinstance(cls, type(pydantic.BaseModel))
+
+
+def does_match(cls, name, asserter):
+    item = getattr(cls, name, None)
+    return asserter(item)
+
+
+class Function(ABC):
+    def __init_subclass__(cls, require_restart: Iterable[str] = None, has_sideeffect: bool = False, **kwargs):
+        super().__init_subclass__(**kwargs)
+
+        cls.has_sideeffect = has_sideeffect
+
+        # if not does_match(cls, "Settings", is_basemodel):
+        #     raise TypeError(f"Class {cls.__name__} must have a BaseModel 'Settings'")
+        #
+        # if not does_match(cls, "Inputs", is_dataclass):
+        #     raise TypeError(f"Class {cls.__name__} must have a dataclass 'Inputs'")
+        #
+        # if not does_match(cls, "Outputs", is_dataclass):
+        #     raise TypeError(f"Class {cls.__name__} must have a dataclass 'Outputs'")
+
+        cls.require_restart = [] if require_restart is None else require_restart
+
+        # TODO: TypeError for typos iun require_restart
 
     def __init__(self, settings):
         self.settings = settings
 
-    @abc.abstractmethod
-    def process(self, inputs, settings=None):
-        pass
+    def __call__(self, settings, inputs):
+        if settings:
+            self.settings = settings
 
+        return self.process(inputs)
+
+    @abstractmethod
+    def process(self, inputs):
+        raise NotImplementedError
 
--- manager.py
+++ manager.py
@@ -1,6 +1,14 @@
-from typing import Dict, Type
+from typing import Dict, Type, Any
 
 from function import Function
+
+
+def fullname(cls: type) -> str:
+    module = cls.__class__.__module__
+    if module is None or module == str.__class__.__module__:
+        return cls.__qualname__
+    else:
+        return module + '.' + cls.__qualname__
 
 
 class Manager:
@@ -9,11 +17,10 @@ class Manager:
     def __init__(self):
         self.idDict = {}
 
-# TODO: Same name collision
-    def register(self, clazz: Type[Function]):
-        self.idDict[clazz.__name__] = clazz
+    def register(self, cls: Type[Function]) -> None:
+        self.idDict[fullname(cls)] = cls
 
-    def create(self, name: str, settings):
+    def create(self, name: str, settings) -> Function:
         return self.idDict[name](settings)
 
 
